ed34d266fe28d76ff6c64f6d88ae7049a2ee9fc0
Vikat Rane <vikat9596@gmail.com>
2024-08-14 15:26:03 +0530

    added courses contact etc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialsCards from "@/components/TestimonialsCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Learn Coding</h1>
+      
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
       <TestimonialsCards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 
 function Navbar({ className }: { className?: string }) {
@@ -12,20 +13,28 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
+        <Link href={"/"}>
         <MenuItem setActive={setActive} active={active} item="Home">
         </MenuItem>
+        </Link>
 
         <MenuItem setActive={setActive} active={active} item="Courses">
           <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
             <HoveredLink href="/web-dev">Web Development</HoveredLink>
             <HoveredLink href="/interface-design">Interface Design</HoveredLink>
             <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
             <HoveredLink href="/branding">Branding</HoveredLink>
           </div>
         </MenuItem>
+        <Link href={"/projects"}>
+        <MenuItem setActive={setActive} active={active} item="Projects">
+           </MenuItem>
+        </Link>
+        <Link href={"/contact"}>
         <MenuItem setActive={setActive} active={active} item="Contact Us">
-          
         </MenuItem>
+        </Link>
         </Menu>
     </div>
   )
